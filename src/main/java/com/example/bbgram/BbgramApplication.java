c8c21927facc36a54bd4ab7171ae3e2c0f15a057
Taro <taro@example.com>
2024-03-14 02:24:11 +0900

    top画面の作成

--- src/main/java/com/example/bbgram/BbgramApplication.java
+++ src/main/java/com/example/bbgram/BbgramApplication.java
@@ -2,12 +2,17 @@ package com.example.bbgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BbgramApplication {
+public class BbgramApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbgramApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BbgramApplication.class);
+	}
 }
